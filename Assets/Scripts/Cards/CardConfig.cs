42129c7007bbc5f2b4b22798e8c08b705a369a6c
Jeffreeyy <17844@ma-web.nl>
2018-03-09 13:51:14 +0100

    Created CardConfig, Added functionality to the PathCard to find the path's nodes

---
+++ Assets/Scripts/Cards/CardConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardConfig
+{
+    public static List<CardData> s_PathCards = new List<CardData>
+    {
+        new CardData(CardTypes.PATH_CARD, "Straight Path", "Miss me with that gay shit", null),
+        new CardData(CardTypes.PATH_CARD, "Straight Path", "Miss me with that gay shit", null),
+        new CardData(CardTypes.PATH_CARD, "Straight Path", "Miss me with that gay shit", null),
+        new CardData(CardTypes.PATH_CARD, "Straight Path", "Miss me with that gay shit", null),
+        new CardData(CardTypes.PATH_CARD, "Straight Path", "Miss me with that gay shit", null)
+    };
+
+    public static List<CardData> s_ActionCards = new List<CardData>
+    {
+        new CardData(CardTypes.ACTION_CARD, "Allahu Akbar", "Blows up a path", null)
+    };
+}
--- Assets/Scripts/Cards/PathCard.cs
+++ Assets/Scripts/Cards/PathCard.cs
@@ -2,11 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PathDirections
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
 public class PathCard : Card
 {
+    public delegate void PathCardValueChanged(PathCard card, List<PathDirections> directions);
+    public static PathCardValueChanged s_OnPathValueChanged;
+
+    private List<PathDirections> m_Directions = new List<PathDirections>();
+    public List<PathDirections> Directions
+    {
+        get { return m_Directions; }
+        set { m_Directions = value; if(s_OnPathValueChanged != null) s_OnPathValueChanged(this, m_Directions); }
+    }
+
     public override void UseCard()
     {
         //effect
         base.UseCard();
+    }
+
+    public Vector2 GetNodeByDirection(PathDirections direction)
+    {
+        switch(direction)
+        {
+            case PathDirections.UP:
+                return Vector2.up;
+            case PathDirections.DOWN:
+                return Vector2.down;
+            case PathDirections.LEFT:
+                return Vector2.left;
+            case PathDirections.RIGHT:
+                return Vector2.right;
+        }
+        Debug.LogError("<color=red>[PathCard.cs]</color> Could not get Node by Direction. (Invalid Direction)");
+        return Vector2.zero;
     }
 }
