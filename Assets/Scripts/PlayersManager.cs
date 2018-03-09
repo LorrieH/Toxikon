8e3beff8c8e11715bc9e87b8e6e79548fd47682c
Lorenzo Hegberg <18927@ma-web.nl>
2018-03-09 15:09:24 +0100

    Started Turnmanager, end turn popup and playersmanager

---
+++ Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour
+{
+    public static PlayersManager s_Instance;
+
+    private List<GameObject> m_Players = new List<GameObject>();
+    public List<GameObject> Players { get { return m_Players; } }
+
+    private void Awake()
+    {
+        if (s_Instance == null)
+        {
+            s_Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    public delegate void TurnEnd();
+    public static TurnEnd s_OnTurnEnd;
+
+    public delegate void TurnAction();
+    public static TurnAction s_OnTurnAction;
+
+    private List<GameObject> m_Players = new List<GameObject>();
+    private int m_CurrentPlayerIndex;
+    private GameObject m_CurrentPlayer;
+
+    private void OnEnable()
+    {
+        s_OnTurnEnd += NextTurn;
+    }
+
+    private void Awake()
+    {
+        m_Players = PlayersManager.s_Instance.Players;
+    }
+
+    void SetFirstTurn()
+    {
+        int randomPlayer = Random.Range(0, m_Players.Count - 1);
+        m_CurrentPlayerIndex = randomPlayer;
+        m_CurrentPlayer = m_Players[m_CurrentPlayerIndex];
+    }
+
+    void NextTurn()
+    {
+        if(m_CurrentPlayerIndex == m_Players.Count - 1)
+        {
+            m_CurrentPlayerIndex = 0;
+            m_CurrentPlayer = m_Players[m_CurrentPlayerIndex];
+        }
+        else
+        {
+            m_CurrentPlayerIndex++;
+            m_CurrentPlayer = m_Players[m_CurrentPlayerIndex];
+        }
+    }
+
+    private void OnDisable()
+    {
+        s_OnTurnEnd -= NextTurn;
+    }
+}
