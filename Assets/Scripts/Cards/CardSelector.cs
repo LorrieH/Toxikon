c5c269df4bdd4206a30ff487e95a3cc982ff625b
Jprummel <jordiprummel@live.nl>
2018-03-13 11:43:24 +0100

    Animation scripts and card animation fix

--- Assets/Scripts/Cards/CardSelector.cs
+++ Assets/Scripts/Cards/CardSelector.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using DG.Tweening;
 
 public class CardSelector : MonoBehaviour {
-    
-    public delegate void ToggleCardSelectEvent(CardData selectedCard, Transform cardTransform,int cardInHandIndex);
+
+    public delegate void ToggleCardSelectEvent(CardData selectedCard, Transform cardTransform, int cardInHandIndex);
     public static ToggleCardSelectEvent s_OnToggleCardSelect;
     [SerializeField] private Transform m_CardHolder;
     [SerializeField] private Transform m_SelectedCardHolder;
@@ -29,34 +29,37 @@ public class CardSelector : MonoBehaviour {
         s_OnToggleCardSelect += ToggleCardSelect;
     }
 
-    private void ToggleCardSelect(CardData selectedCard,Transform cardTransform,int cardInHandIndex)
+    private void ToggleCardSelect(CardData selectedCard, Transform cardTransform, int cardInHandIndex)
     {
         SelectCard(selectedCard, cardTransform, cardInHandIndex);
         return;
-        for (int i = 0; i < PlayersManager.s_Instance.Players[i].PlayerHand.Cards.Count; i++)
-        {
-            if (PlayersManager.s_Instance.Players[i].PlayerHand.Cards[i] == selectedCard)
-            {
-                SelectCard(selectedCard, cardTransform,cardInHandIndex);
-            }
-            else if (PlayersManager.s_Instance.Players[i].PlayerHand.Cards[i] != selectedCard)
-            {
-
-            }
-        }
     }
 
-    private void SelectCard(CardData selectedCard, Transform cardTransform,int cardInHandIndex)
+    private void SelectCard(CardData selectedCard, Transform cardTransform, int cardInHandIndex)
     {
+        
+        //Returns all cards back to the hand first
+        for (int i = 0; i < m_PlayerHandCards.Count; i++)
+        {
+            Sequence CardDeselectSequence = DOTween.Sequence();
+
+            CardDeselectSequence.Append(m_PlayerHandCards[i].transform.DOScale(1, 0.2f));
+            m_PlayerHandCards[i].transform.SetParent(m_CardHolder);
+            m_PlayerHandCards[i].transform.SetSiblingIndex(cardInHandIndex);
+            m_SelectedCard = null;
+
+        }
+
         if (m_SelectedCard != selectedCard)
         {
             Sequence CardSelectSequence = DOTween.Sequence();
             cardTransform.SetParent(m_SelectedCardHolder);
             CardSelectSequence.Append(cardTransform.DOMove(m_SelectedCardHolder.position, 0.3f));
             CardSelectSequence.Append(cardTransform.DOScale(1.3f, 0.2f));
-            m_SelectedCard = selectedCard;  
+            m_SelectedCard = selectedCard;
         }
-        else {
+        else if (m_SelectedCard == selectedCard)
+        {
             Sequence CardDeselectSequence = DOTween.Sequence();
 
             CardDeselectSequence.Append(cardTransform.DOScale(1, 0.2f));
@@ -65,6 +68,7 @@ public class CardSelector : MonoBehaviour {
             m_SelectedCard = null;
         }
     }
+
 
     private void DeselectCard(Transform cardTransform,Vector2 defaultPosition,int cardInHandIndex)
     {
