f3164dc4d3dd80f15448a321e77641c541e70d04
Jprummel <jordiprummel@live.nl>
2018-03-13 11:44:18 +0100

    Animation Scripts

---
+++ Assets/Scripts/Animations/AnimationPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AnimationPlayer : MonoBehaviour {
+
+    private Animator _animator;
+
+	public void PlayTriggerAnimation(string animationString)
+    {
+        _animator.SetTrigger(animationString);
+    }
+
+    public void SetAnimationBool(string animationString, bool boolValue)
+    {
+        _animator.SetBool(animationString, boolValue);
+    }
+}
