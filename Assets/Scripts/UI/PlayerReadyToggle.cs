562633169d25285487b7242166bdbd0f845830b2
Lorenzo Hegberg <18927@ma-web.nl>
2018-03-14 12:53:55 +0100

    Added character select ready check

---
+++ Assets/Scripts/UI/PlayerReadyToggle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerReadyToggle : MonoBehaviour
+{
+    private Toggle m_Toggle;
+
+    private void Start()
+    {
+        m_Toggle = GetComponent<Toggle>();
+
+        m_Toggle.onValueChanged.AddListener(delegate { ToggleChangedValue(); });
+    }
+
+    void ToggleChangedValue()
+    {
+        if (m_Toggle.isOn)
+            ReadyCheck.s_Instance.ChangeAmountOfChecks(1);
+        else
+            ReadyCheck.s_Instance.ChangeAmountOfChecks(-1);
+
+        Debug.Log("Checks needed: " + ReadyCheck.s_Instance.ChecksNeeded + " Current checks: " + ReadyCheck.s_Instance.CurrentChecks);
+    }
+}
