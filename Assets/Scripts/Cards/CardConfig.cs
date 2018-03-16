1f550b100b095d354742f5c0d7c67f8ceb885fd5
Jprummel <jordiprummel@live.nl>
2018-03-16 09:10:03 +0100

    small card update

--- Assets/Scripts/Cards/CardConfig.cs
+++ Assets/Scripts/Cards/CardConfig.cs
@@ -12,7 +12,7 @@ public class CardConfig
         new CardData(CardTypes.PATH_CARD, "Left Up Corner Path", "Creates a Left Up Corner Path", null, null, new PathCardData(true, false, true, false, true)),
         new CardData(CardTypes.PATH_CARD, "Left Down Corner Path", "Creates a Left Down Corner Path", null, null, new PathCardData(false, true, true, false, true)),
         new CardData(CardTypes.PATH_CARD, "Right Up Corner Path", "Creates a Right  Up Corner Path", null, null, new PathCardData(true, false, false, true, true)),
-        new CardData(CardTypes.PATH_CARD, "Right Down Corner Path", "Creates a Left Up Corner Path", null, null, new PathCardData(false, true, false, true, true)),
+        new CardData(CardTypes.PATH_CARD, "Right Down Corner Path", "Creates a Right Up Corner Path", null, null, new PathCardData(false, true, false, true, true)),
         new CardData(CardTypes.PATH_CARD, "T-Split Up", "Creates a T Split Up Path", null, null, new PathCardData(true,false,true,true,true)),
         new CardData(CardTypes.PATH_CARD, "T-Split Down", "Creates a T Split Down Path", null, null, new PathCardData(false,true,true,true,true)),
         new CardData(CardTypes.PATH_CARD, "T-Split Left", "Creates a T Split Left Path", null, null, new PathCardData(true,true,true,false,true)),
