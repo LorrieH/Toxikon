fd414ed6638a5e308b61a30105a9e36f8b03ce0b
Jprummel <jordiprummel@live.nl>
2018-03-16 14:39:36 +0100

    Card names edited

--- Assets/Scripts/Cards/CardConfig.cs
+++ Assets/Scripts/Cards/CardConfig.cs
@@ -7,12 +7,12 @@ public class CardConfig
     public static List<CardData> s_PathCards = new List<CardData>
     {
         new CardData(CardTypes.PATH_CARD, "Crossroad", "Creates a Crossroad Path", null, null, new PathCardData(true,true,true,true,true)),
-        new CardData(CardTypes.PATH_CARD, "Straight Horizontal Path", "Straight Horizontal Path", null, null, new PathCardData(false, false, true, true, true)),
-        new CardData(CardTypes.PATH_CARD, "Straight Vertical Path", "Straight Vertical Path", null, null, new PathCardData(true, true, false, false, true)),
-        new CardData(CardTypes.PATH_CARD, "Left Up Corner Path", "Creates a Left Up Corner Path", null, null, new PathCardData(true, false, true, false, true)),
-        new CardData(CardTypes.PATH_CARD, "Left Down Corner Path", "Creates a Left Down Corner Path", null, null, new PathCardData(false, true, true, false, true)),
-        new CardData(CardTypes.PATH_CARD, "Right Up Corner Path", "Creates a Right  Up Corner Path", null, null, new PathCardData(true, false, false, true, true)),
-        new CardData(CardTypes.PATH_CARD, "Right Down Corner Path", "Creates a Right Up Corner Path", null, null, new PathCardData(false, true, false, true, true)),
+        new CardData(CardTypes.PATH_CARD, "Straight Horizontal", "Straight Horizontal Path", null, null, new PathCardData(false, false, true, true, true)),
+        new CardData(CardTypes.PATH_CARD, "Straight Vertical", "Straight Vertical Path", null, null, new PathCardData(true, true, false, false, true)),
+        new CardData(CardTypes.PATH_CARD, "Corner Left Up", "Creates a Left Up Corner Path", null, null, new PathCardData(true, false, true, false, true)),
+        new CardData(CardTypes.PATH_CARD, "Corner Left Down", "Creates a Left Down Corner Path", null, null, new PathCardData(false, true, true, false, true)),
+        new CardData(CardTypes.PATH_CARD, "Corner Right Up", "Creates a Right  Up Corner Path", null, null, new PathCardData(true, false, false, true, true)),
+        new CardData(CardTypes.PATH_CARD, "Corner Right Down", "Creates a Right Up Corner Path", null, null, new PathCardData(false, true, false, true, true)),
         new CardData(CardTypes.PATH_CARD, "T-Split Up", "Creates a T Split Up Path", null, null, new PathCardData(true,false,true,true,true)),
         new CardData(CardTypes.PATH_CARD, "T-Split Down", "Creates a T Split Down Path", null, null, new PathCardData(false,true,true,true,true)),
         new CardData(CardTypes.PATH_CARD, "T-Split Left", "Creates a T Split Left Path", null, null, new PathCardData(true,true,true,false,true)),
