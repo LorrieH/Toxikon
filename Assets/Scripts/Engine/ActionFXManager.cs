8cbf183478154ac744b7673f02825acb53426901
Jeffreeyy <17844@ma-web.nl>
2018-03-20 13:34:22 +0100

    Created the ActionFX Manager

---
+++ Assets/Scripts/Engine/ActionFXManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionFXManager : MonoBehaviour
+{
+    public static ActionFXManager s_Instance;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        if (s_Instance == null)
+            s_Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void SwapTiles(TileNode tile1, TileNode tile2)
+    {
+
+    }
+
+    public void DestroyTile(TileNode tile)
+    {
+
+    }
+
+    public void RotateTile(TileNode tile)
+    {
+
+    }
+}
