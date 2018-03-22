2940df3e76ef2afdc2362f3b61b8ca71e40a2f79
Jprummel <jordiprummel@live.nl>
2018-03-22 01:15:46 +0100

    - Temporary fix for card drawing and switching the card data (will be refactored and changed later)

--- Assets/Scripts/Cards/CardPositionHolder.cs
+++ Assets/Scripts/Cards/CardPositionHolder.cs
@@ -48,14 +48,17 @@ public class CardPositionHolder : MonoBehaviour {
 
     public void DiscardCard(Card card)
     {
+        CardSelector.s_Instance.SelectedCard.CardData = CardManager.s_Instance.GetRandomCard();
         card.gameObject.SetActive(false);
-        CardSelector.s_Instance.SelectedCard = null;
+        //TurnManager.s_Instance.CurrentPlayer.PlayerData.Cards[m_IndexInHandPosition] = CardManager.s_Instance.GetRandomCard();
+        TurnManager.s_Instance.CurrentPlayer.PlayerData.Cards[m_IndexInHandPosition] = CardSelector.s_Instance.SelectedCard.CardData;
+        card.SetCardInfo();
+        //CardSelector.s_Instance.SelectedCard = null;
         card.transform.DOMove(m_CardDeckPosition.position, 0.1f);
         card.transform.DOScale(0.7f, 0.1f);
         m_SelectedCard = card;
         m_IndexInHandPosition = m_SelectedCard.IndexInHand;
-        TurnManager.s_Instance.CurrentPlayer.PlayerData.Cards[m_IndexInHandPosition] = CardManager.s_Instance.GetRandomCard();
-        DrawCard();
+        StartCoroutine(DrawCardRoutine());
     }
 
     public void DrawCard()
@@ -68,6 +71,9 @@ public class CardPositionHolder : MonoBehaviour {
         CardSelector.s_Instance.CanSelectCard = false;
         yield return new WaitForSeconds(0.5f);
         m_SelectedCard.gameObject.SetActive(true);
+        TurnManager.s_Instance.CurrentPlayer.PlayerData.Cards[m_IndexInHandPosition] = CardSelector.s_Instance.SelectedCard.CardData;
+        //CardSelector.s_Instance.SelectedCard.SetCardInfo();
+        CardSelector.s_Instance.SelectedCard = null;
         m_SelectedCard.transform.DOMove(m_ShowDrawnCardPosition.position, 0.5f);
         m_SelectedCard.transform.DOScale(1.5f, 0.5f);
         yield return new WaitForSeconds(1.2f);
