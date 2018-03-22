6e361d833af448bbfcb6d4a17a49fa475f3431a3
Sjoerd Jakobs <sjakobs@chello.nl>
2018-03-22 13:15:47 +0100

    burned bridges

--- Assets/Scripts/Grids&Tiles/TileGrid.cs
+++ Assets/Scripts/Grids&Tiles/TileGrid.cs
@@ -13,7 +13,7 @@ public class TileGrid : MonoBehaviour
     [SerializeField] private GameObject m_NodeObjectPrefab;
 
     private TileNode[,] m_NodeGrid;
-    private GameObject[,] m_NodeBridges;
+    private Bridges[,] m_NodeBridges;
 
 
     private TileNode[] m_PlayerStartNodes;
@@ -52,13 +52,14 @@ public class TileGrid : MonoBehaviour
         int amountOfPlayersPlaceholder = 4;
         Debug.LogError("the amount of players still needs to be recieved");
         m_PlayerStartNodes = new TileNode[amountOfPlayersPlaceholder];
-        m_NodeBridges = new GameObject[m_GridXSize, m_GridYSize];
+        m_NodeBridges = new Bridges[m_GridXSize + 2, m_GridYSize + 2];
         m_NodeGrid = new TileNode[m_GridXSize + 2, m_GridYSize + 2];
         for (int i = 0; i < m_GridXSize + 2; i++)
         {
             for (int j = 0; j < m_GridYSize + 2; j++)
             {
                 TileNode newNode = new TileNode();
+                Bridges newbridge = new Bridges();
                 TileBools bools = new TileBools();
                 newNode.IsFilled = false;
                 newNode.IsDestructable = true;
@@ -136,6 +137,7 @@ public class TileGrid : MonoBehaviour
                 newNode.Bools = bools;
                 newNode.UpdateArt();
                 m_NodeGrid[i, j] = newNode;
+                m_NodeBridges[i,j] = newbridge;
             }
         }
     }
@@ -225,7 +227,8 @@ public class TileGrid : MonoBehaviour
             else
             {
                 m_NodeGrid[x, y].UpdateArt();
-            }            
+            }
+            StartCoroutine(DestroyBridges(m_NodeBridges[x, y], delay));
             return true;
         }
         else
@@ -237,6 +240,26 @@ public class TileGrid : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         T.UpdateArt();
+    }
+    IEnumerator DestroyBridges(Bridges B, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if(B.bridgeUp != null)
+        {
+            Destroy(B.bridgeUp);
+        }
+        if (B.bridgeRight != null)
+        {
+            Destroy(B.bridgeRight);
+        }
+        if (B.bridgeDown != null)
+        {
+            Destroy(B.bridgeDown);
+        }
+        if (B.bridgeLeft != null)
+        {
+            Destroy(B.bridgeLeft);
+        }
     }
 
     public bool PlaceNewCard(int x, int y, bool UpCard, bool RightCard, bool DownCard, bool LeftCard, bool MiddleCard, float delay = 0)
@@ -263,22 +286,26 @@ public class TileGrid : MonoBehaviour
             if (UpCard && m_NodeGrid[x, y].AllNeighbours.up.IsFilled)
             {
                 GameObject Bridge = Instantiate(TileArtLib.s_Bridges[0], new Vector3(x , y + 0.5f, 0), Quaternion.identity, m_NodeGrid[x,y].TileObject.transform);
-                //m_NodeBridges[] = Bridge;
+                m_NodeBridges[x, y].bridgeUp = Bridge;
+                m_NodeBridges[x, y+1].bridgeDown = Bridge;
             }
             if (RightCard && m_NodeGrid[x, y].AllNeighbours.right.IsFilled)
             {
                 GameObject Bridge = Instantiate(TileArtLib.s_Bridges[1], new Vector3(x +0.5f, y, 0), Quaternion.identity, m_NodeGrid[x, y].TileObject.transform);
-                //m_NodeBridges[] = Bridge;
+                m_NodeBridges[x, y].bridgeRight = Bridge;
+                m_NodeBridges[x+1, y].bridgeLeft = Bridge;
             }
             if (DownCard && m_NodeGrid[x, y].AllNeighbours.down.IsFilled)
             {
                 GameObject Bridge = Instantiate(TileArtLib.s_Bridges[0], new Vector3(x , y- 0.5f, 0), Quaternion.identity, m_NodeGrid[x, y].TileObject.transform);
-                //m_NodeBridges[] = Bridge;
+                m_NodeBridges[x, y].bridgeDown = Bridge;
+                m_NodeBridges[x, y - 1].bridgeUp = Bridge;
             }
             if (LeftCard && m_NodeGrid[x, y].AllNeighbours.left.IsFilled)
             {
                 GameObject Bridge = Instantiate(TileArtLib.s_Bridges[1], new Vector3(x - 0.5f, y, 0), Quaternion.identity, m_NodeGrid[x, y].TileObject.transform);
-                //m_NodeBridges[] = Bridge;
+                m_NodeBridges[x, y].bridgeLeft = Bridge;
+                m_NodeBridges[x - 1, y].bridgeRight = Bridge;
             }
             return true;
         }
@@ -305,7 +332,10 @@ public class TileGrid : MonoBehaviour
     #endregion
 }
 
-public struct Bridges
+public class Bridges
 {
-
+    public GameObject bridgeUp;
+    public GameObject bridgeRight;
+    public GameObject bridgeDown;
+    public GameObject bridgeLeft;
 }
