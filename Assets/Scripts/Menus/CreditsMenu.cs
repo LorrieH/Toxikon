96e5ea4310e9015364f15cc80d99372c802dd781
Jprummel <jordiprummel@live.nl>
2018-03-22 10:36:26 +0100

    - Added credits - Edited menu UI

---
+++ Assets/Scripts/Menus/CreditsMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenu : Menu {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void Show()
+    {
+        base.Show();
+    }
+
+    public override void Hide()
+    {
+        base.Hide();
+    }
+}
