b3c471d7413b89e6793fa29ddf0710f9aaaf4d34
Jeffreeyy <17844@ma-web.nl>
2018-03-22 13:51:58 +0100

    Updated the background, Changed tile size to 0.9 and created a tileclick manager

---
+++ Assets/Scripts/Engine/TileClickManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileClickManager : MonoBehaviour
+{
+    public static TileClickManager s_Instance;
+
+    private List<Vector2> m_ClickedTilePositions = new List<Vector2>();
+    public List<Vector2> ClickedTilePositions { get { return m_ClickedTilePositions; } set { m_ClickedTilePositions = value; } }
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        if (s_Instance == null)
+            s_Instance = this;
+        else
+            Destroy(gameObject);
+    }
+}
